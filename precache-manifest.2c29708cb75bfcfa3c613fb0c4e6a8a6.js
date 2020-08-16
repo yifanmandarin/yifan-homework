2c5484111f0c4c1549a340800d4e38e230b38b4b
yifanmandarin <yifanmandarin6@gmail.com>
2020-08-16 13:49:12 +0800

    Updates

---
+++ precache-manifest.2c29708cb75bfcfa3c613fb0c4e6a8a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11adb7e011142cfeb4b5e11622adb3ae",
+    "url": "/yifan-homework/index.html"
+  },
+  {
+    "revision": "9816d5cad291d750f0e2",
+    "url": "/yifan-homework/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c168018b36f9ecae918b",
+    "url": "/yifan-homework/static/js/2.1a084476.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yifan-homework/static/js/2.1a084476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9816d5cad291d750f0e2",
+    "url": "/yifan-homework/static/js/main.8e878a33.chunk.js"
+  },
+  {
+    "revision": "e7dd94b3feb430b21e47",
+    "url": "/yifan-homework/static/js/runtime-main.c5ffd070.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/yifan-homework/static/media/logo.5d5d9eef.svg"
+  }
+]);
